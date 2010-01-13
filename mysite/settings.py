3cb7cf2b126df71ceb1a66a7a3e18d1e3c200868
Dougal Matthews <dougal85@gmail.com>
2010-01-13 14:47:18 +0000

    part 2 and removed database

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,13 +66,12 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    'templates'
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
